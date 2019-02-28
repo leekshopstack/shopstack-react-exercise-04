ab2497708e61c1379d7a8554a2893f51f6b6e676
Leek <tuan@shopstack.asia>
2019-02-28 13:59:41 +0700

    Updates

---
+++ precache-manifest.cd41f501fe4d603ba8fe1aff5181517c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91ee6d86d7a4cd23106d",
+    "url": "/shopstack-react-exercise-04/static/css/main.a4bd9610.chunk.css"
+  },
+  {
+    "revision": "91ee6d86d7a4cd23106d",
+    "url": "/shopstack-react-exercise-04/static/js/main.91ee6d86.chunk.js"
+  },
+  {
+    "revision": "e279d281e6b6a3925024",
+    "url": "/shopstack-react-exercise-04/static/js/runtime~main.e279d281.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/shopstack-react-exercise-04/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "a8948786fbd0be5f554e64a3faca6f45",
+    "url": "/shopstack-react-exercise-04/static/media/avatar.a8948786.png"
+  },
+  {
+    "revision": "468da4bae05a4e964831f9db1fc6f6f8",
+    "url": "/shopstack-react-exercise-04/index.html"
+  }
+];
